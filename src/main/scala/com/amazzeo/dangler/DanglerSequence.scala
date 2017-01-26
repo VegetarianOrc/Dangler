276698c6f5b5090de2982da12d14b9e359801b3e
Alex Mazzeo <alex.mazzeo@adcolony.com>
2017-01-26 08:23:47 -0800

    refactor a bit. Add intro countdown.

--- src/main/scala/com/amazzeo/dangler/DanglerSequence.scala
+++ src/main/scala/com/amazzeo/dangler/DanglerSequence.scala
@@ -5,25 +5,27 @@ import scala.concurrent.duration._
 import scala.util.Random
 
 class DanglerSequence(interval: FiniteDuration, duration: FiniteDuration) {
-  val Ending = "Workout ended."
+  val INTRO = Seq(
+    Node("Starting workout in 3"),
+    BreakNodes.XStrongBreakNode(1.second),
+    Node("2"),
+    BreakNodes.XStrongBreakNode(1.second),
+    Node("1")
+    )
+
+  val ENDING = Seq(BreakNodes.XStrongBreakNode(1.second), Node("Workout ended."))
 
   val doc: SsmlDoc = {
     val d = new SsmlDoc()
+    d ++ INTRO
     val numIntervals = duration.toMillis / interval.toMillis
     for (i <- 0 to numIntervals.toInt) {
-      Random.nextInt(3) match {
-        case 0 => d.addNode(ColorNodes.Red)
-        case 1 => d.addNode(ColorNodes.Green)
-        case 2 => d.addNode(ColorNodes.Yellow)
-      }
-
       if ( i < numIntervals) {
-        d.addNode(BreakNode(XStrongBreak, interval))
-      } else {
-        d.addNode(BreakNode(XStrongBreak, 1.second))
-        d.addNode(PlainTextNode(Ending))
+        d :+ ColorNodes.getRandom()
+        d :+ BreakNodes.XStrongBreakNode(interval)
       }
     }
+    d ++ ENDING
     d
   }
 
--- src/main/scala/com/amazzeo/dangler/speechutils/SsmlDoc.scala
+++ src/main/scala/com/amazzeo/dangler/speechutils/SsmlDoc.scala
@@ -1,36 +1,55 @@
 package com.amazzeo.dangler.speechutils
 
 import scala.concurrent.duration._
+import scala.util.Random
 
-trait Node
-
-case class BreakNode( strength: BreakStrength, time: FiniteDuration) extends Node {
-  override def toString() = s"""<break strength="${strength}" time="${time.toMillis}ms"/>"""
+object Node {
+  def apply(text:String) = new Node(text)
 }
 
-object ColorNodes {
-  sealed trait ColorNode extends Node
-  case object Red extends ColorNode {
-    override def toString() = "red"
-  }
-
-  case object Green extends ColorNode {
-    override def toString() = "green"
-  }
-
-  case object Yellow extends ColorNode {
-    override def toString() = "yellow"
-  }
-}
-
-case class PlainTextNode(text: String) extends Node {
+class Node(text:String) {
   override def toString() = text
 }
 
-class SsmlDoc {
-  private var children: Seq[Node] = Seq()
+object BreakNodes {
+  class BreakNode( strength: BreakStrength, time: FiniteDuration) extends Node(s"""<break strength="${strength}" time="${time.toMillis}ms"/>""")
 
-  def addNode(n: Node) = {
+  case object NoBreakNode extends BreakNode(NoBreak,0.seconds)
+  case class XWeakBreakNode(duration: FiniteDuration) extends BreakNode(XWeakBreak, duration)
+  case class WeakBreakNode(duration: FiniteDuration) extends BreakNode(WeakBreak, duration)
+  case class MediumBreakNode(duration: FiniteDuration) extends BreakNode(MediumBreak, duration)
+  case class StrongBreakNode(duration: FiniteDuration) extends BreakNode(StrongBreak, duration)
+  case class XStrongBreakNode(duration: FiniteDuration) extends BreakNode(XStrongBreak, duration)
+}
+
+
+
+object ColorNodes {
+  case object Red extends Node("red")
+
+  case object Green extends Node("green")
+
+  case object Yellow extends Node("yellow")
+
+  private val values = Array(Red, Green, Yellow)
+
+  def getRandom(): Node = {
+    values(Random.nextInt(3))
+  }
+}
+
+
+object SsmlDoc {
+  def apply(children: Seq[Node] = Seq.empty[Node]) = new SsmlDoc(children)
+}
+
+class SsmlDoc(var children:Seq[Node] = Seq.empty[Node]) {
+
+  def ++(s: Seq[Node]) = {
+    children = (children ++ s)
+  }
+
+  def :+(n: Node) = {
     children = children :+ n
   }
 
